5b145835dd06e9dc6f683526c7c372c3b2563fc2
Marc Muehlheim <marc.muehlheim@hftm.ch>
2023-06-16 18:01:11 +0200

    Remove safe area stuff, overrated

--- src/components/chat-input/ChatInput.tsx
+++ src/components/chat-input/ChatInput.tsx
@@ -57,7 +57,7 @@ const ChatInput = ({ role, appendToChat }: Props) => {
   }, [isLoading, focus]);
 
   return (
-    <div className="mt-2 mb-3 input-style safe-area-bottom-padding">
+    <div className="mt-2 mb-3 input-style">
       <input
         autoFocus={true}
         ref={inputRef}
