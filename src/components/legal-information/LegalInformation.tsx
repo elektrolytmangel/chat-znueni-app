94b5562e7af3f909d8253e9038d90f0dd399d2b7
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2023-09-05 21:27:40 +0200

    Add build number

--- src/components/legal-information/LegalInformation.tsx
+++ src/components/legal-information/LegalInformation.tsx
@@ -3,9 +3,11 @@ import './LegalInformation.css';
 
 const LegalInformation = () => {
   const { t } = useTranslation();
+  const version = process.env.REACT_APP_VERSION || '0.0.0';
+
   return (
     <div>
-      <p className="text-sm">{t('legal_information')}</p>
+      <p className="text-sm">{t('legal_information')} v{version}</p>
     </div>
   )
 }
